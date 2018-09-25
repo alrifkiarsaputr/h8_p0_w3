3ae7f9293f45ce2d78679ab2f3eb307ee3a0f504
Alrifki Arsa Putra <putraalrifki@gmail.com>
2018-09-25 15:22:23 +0700

    revisi exercises 4 tantangan array 3

--- Exercises_4.js
+++ Exercises_4.js
@@ -1,31 +1,42 @@
-function dataHandling2(params) {
-  var paramName = params.slice(1, 2).toString();
+function dataHandling2(input) {
+  input.splice(1,2,'Roman Alamsyah Elsharawy','Provinsi Bandar Lampung')
+  input.splice(4,1,'Pria','SMA Internasional Metro')
   
-  params = params.map(item => {
-            if (item === paramName) {
-              item = paramName.concat('Elsharawy');
-            }
-            if (item === params.slice(2, 3).toString()) {
-              item = 'Provinsi '.concat(params.slice(2, 3).toString());
-            }
-            return item;
-          });
-  params.splice(4, 0, 'Pria', 'SMA Internasional Metro');
-  params.pop();
-  console.log(params)
+  console.log(input) 
   
-  switch (Number(params.slice(3, 4).toString().split('/')[1])) {
-    case 1: console.log('Januari'); break;
-    case 2: console.log('Februari'); break;
-    case 3: console.log('Maret'); break;
-    case 4: console.log('April'); break;
-    case 5: console.log('Mei'); break;
+  var tanggal = input[3].split('/')
+  
+  var tanggal2 = tanggal.join('-')
+  // return tanggal2
+  // console.log(tanggal2);
+
+  // return tanggal[1]
+  var bulan = tanggal[1]
+  switch (bulan) {
+      case '01': {console.log('Januari'); break;}
+      case '02': {console.log('Februari'); break;}
+      case '03': {console.log('Maret'); break;}
+      case '04': {console.log('April'); break;}
+      case '05': {console.log('Mei'); break;}
+      case '06': {console.log('Juni'); break;}
+      case '07': {console.log('Juli'); break;}
+      case '08': {console.log('Agustus'); break;}
+      case '09': {console.log('September'); break;}
+      case '10': {console.log('Oktober'); break;}
+      case '11': {console.log('November'); break;}
+      case '12': {console.log('Desember'); break;}
   }
   
-  console.log(params.slice(3, 4).toString().split('/'));
-  console.log(params.slice(3, 4).toString().split('/').join().replace(/,/g, '-'));
-  console.log(paramName);
+  // tanggal.parseInt()
+  tanggal.sort(function(value1, value2) { return Number(value1) < Number(value2) })
+
+  console.log(tanggal);
+  console.log(tanggal2);
+  
+  return input[1].slice(0,14)
+  // return input[1]
+  // return tanggal
 }
 
-var input = ["0001", "Roman Alamsyah ", "Bandar Lampung", "21/05/1989", "Membaca"];
-dataHandling2(input);
+console.log(dataHandling2(["0001", "Roman Alamsyah", "Bandar Lampung", "21/05/1989", "Membaca"] ));
+
